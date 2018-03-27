ff06c3192a6f0ac8dd5c2134a4b7b2abded19d56
William <gcphost@gmail.com>
2018-03-26 17:40:35 -0700

    Initial commit

---
+++ Providers/HashProvider.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const {
+  ServiceProvider
+} = require('adonis-fold');
+
+class HashProvider extends ServiceProvider {
+  /**
+   * Register method called by the Ioc container
+   * to register the provider
+   *
+   * @method register
+   *
+   * @return {void}
+   */
+  * register() {
+    this.app.singleton('Lesswork/Hash', function (app) {
+      const Hash = require('../index');
+      const key = env('APP_KEY');
+
+      if (!key) {
+        throw `Missing 'APP_KEY' in your '.env.js' file.`;
+      }
+
+      return new Hash(key);
+    });
+  }
+}
+
+module.exports = HashProvider;
---
+++ README.md
@@ -0,0 +1,30 @@
+# lesswork-hash
+This is a hashids provider for the `lesswork-framework`.
+
+# Installation
+```bash 
+npm i --save lesswork-hashids
+```
+
+Register the provider and alias in your `config/app.js` file.
+
+```js
+const providers = [
+  ...
+  'lesswork-hashids/Providers/HashProvider',
+];
+
+const aliases = {
+  ...
+  Hash: 'Lesswork/Hash',
+};
+```
+
+# Usage
+See (hashids)[https://www.npmjs.com/package/hashids] for full usage information.
+
+```js
+const Hash = use('Hash');
+
+console.log(Hash.encode(1));
+```
